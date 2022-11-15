78cbd665175ed964e769cf6ca1f72c5c74a60a0c
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-15 20:51:20 +0600

    setup mongo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 var jwt = require('jsonwebtoken');
 var cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -14,6 +16,23 @@ const port = process.env.PORT || 5000
 app.get('/', (req, res) => {
     res.send("Doctors's portal is running......");
 })
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7incky7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Doctors's portal is running on port ${port}`)
