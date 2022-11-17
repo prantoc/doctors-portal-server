74adcd531200fcfc7f1103612ba89e1d6dbcd131
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-17 21:55:32 +0600

    jwt token api

--- index.js
+++ index.js
@@ -89,14 +89,16 @@ async function run() {
         app.get('/jwt', async (req, res) => {
             const email = req.query.email
             const query = { email: email }
-            const user = await usersCollection.find(query);
+            const user = await usersCollection.findOne(query);
             if (user) {
-                // { algorithm: 'RS256' },
                 const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
-                res.send({ accessToken: token })
+                return res.send({ accessToken: token })
             }
             res.status(403).send({ accessToken: '' })
         })
+
+
+
         //? Users
         app.post('/users', async (req, res) => {
             const user = req.body
